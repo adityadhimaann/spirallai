fdfd88f28420c715c1cb216479dc897d04e2444a
Aditya <dhimanaditya56@gmail.com>
2026-07-09 11:09:02 +0530

    fix: bypass local antivirus self-signed certificate interception for API calls

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import "dotenv/config";
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // Bypass local antivirus/proxy self-signed cert interception
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
